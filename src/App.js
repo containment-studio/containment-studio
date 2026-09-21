c6b5f25bbc32a027a7011e92d559dce856aba0f1
netdenizenLain <netdenizenlain@gmail.com>
2026-09-21 19:57:20 -0400

    Update App.js
    Test commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           CONTAINMENT STUDIO
+
+          UNDER DEVELOPMENT
         </p>
         <script src="https://code.jquery.com/pep/0.4.3/pep.js"></script>
         
